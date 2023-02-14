f950db869c3a406cc42532a348e10e3a93815858
Savidya Sathyani <mails.savidya@gmail.com>
2023-02-14 17:27:43 +1100

    Add validations

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,39 +1,110 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    unique: true,
-    required: [true, 'A tour must have a name.'],
-    trim: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: [true, 'A tour must have a name.'],
+      maxlength: [40, 'A tour must have less or equal than 40 characters'],
+      minlength: [10, 'A tour must have more or equal than 10 characters'],
+      trim: true,
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration.'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size.'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty level.'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty should be one of easy, medium, or difficult',
+      },
+    },
+    price: { type: Number, required: [true, 'A tour must have a price.'] },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (v) {
+          return v < this.price;
+        },
+        message: (props) => `${props.value} is not a valid discount!`,
+      },
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating should be in between 1.0 and 5.0'],
+      max: [5, 'Rating should be in between 1.0 and 5.0'],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    summary: {
+      type: String,
+      trim: true,
+      require: [true, 'A tour must have a description'],
+    },
+    description: { type: String, trim: true },
+    imageCover: { type: String, required: [true, 'A tour must have an image'] },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+    },
   },
-  duration: { type: Number, required: [true, 'A tour must have a duration.'] },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size.'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty level.'],
-  },
-  price: { type: Number, required: [true, 'A tour must have a price.'] },
-  ratingsAverage: { type: Number, default: 4.5 },
-  ratingsQuantity: { type: Number, default: 0 },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    require: [true, 'A tour must have a description'],
-  },
-  description: { type: String, trim: true },
-  imageCover: { type: String, required: [true, 'A tour must have an image'] },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-  startDates: [Date],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// Virtual fields that will not save in DB
+tourSchema.virtual('durationInWeeks').get(function () {
+  return this.duration / 7;
+});
+
+// Document middleware - pre and post (this will give access to current document)
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre(/^find/, function (next) {
+  this.startTime = Date.now();
+  this.find({ secretTour: { $ne: true } });
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(
+    `Query took ${Date.now() - this.startTime} milliseconds to execute.`
+  );
+  next();
+});
+
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({
+    $match: {
+      secretTour: {
+        $ne: true,
+      },
+    },
+  });
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
