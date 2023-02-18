a0bf91835e4e982872f62e69011f4660ee786279
Savidya Sathyani <mails.savidya@gmail.com>
2023-02-18 16:36:58 +1100

    Improve security

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const userRouter = require('./routes/userRouter');
@@ -7,17 +12,60 @@ const tourRouter = require('./routes/tourRouter');
 
 const app = express();
 
-// Middleware
+// Global Middleware
 // The order that we defines middleware is matters.
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); // Logger
 }
+
+// To serve static file
 app.use(express.static(`${__dirname}/public`));
+
+// In order to access the data in the request body
+app.use(
+  express.json({
+    limit: '10kb',
+  })
+);
+
+// Data sanitize against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitize against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// Set rate limit to prevent Brute force and DOS attacks
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP. Please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Log request time
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  next(); // next() is a must code otherwise the request will get stuck in the middle
+  next();
 });
-app.use(express.json()); // In order to access the data in the request body
 
 // Mounting routers
 app.use('/api/v1/tours', tourRouter);
@@ -27,7 +75,6 @@ app.use('/api/v1/users', userRouter);
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404));
 });
-
 app.use(globalErrorHandler);
 
 module.exports = app;
